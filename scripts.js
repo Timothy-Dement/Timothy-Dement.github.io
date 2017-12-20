adfe24c5d1043607b3b353eb7c1209d33e62f560
Timothy-Dement <tmdement@ncsu.edu>
2017-12-19 19:29:30 -0500

    Remove focus

--- scripts.js
+++ scripts.js
@@ -25,16 +25,16 @@ $('a[href*="#"]')
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 1000, function() {
+        }, 500, function() {
           // Callback after animation
           // Must change focus!
           var $target = $(target);
-          $target.focus();
+          // $target.focus();
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
           } else {
             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
+            // $target.focus(); // Set focus again
           };
         });
       }
